29dc32d2ca0448e31e6dbae150d9c7acefe4f52e
LukaszJal <67760453+LukaszJal@users.noreply.github.com>
2022-10-24 23:43:42 +0200

    configuration in application.yml @Autowired

---
+++ src/main/java/eu/jaloszynski/todoapp/controller/InfoControler.java
@@ -0,0 +1,26 @@
+package eu.jaloszynski.todoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class InfoControler {
+
+    @Autowired
+    private DataSourceProperties dataSource;
+    @Value("${my.prop}")
+    private String myProp;
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    String myProp() {
+        return myProp;
+    }
+}
